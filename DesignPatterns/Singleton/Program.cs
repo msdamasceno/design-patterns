1c46c8f77bcdee7c008e5a567b59cbeebbb15630
Marcelo Damasceno <marcelo.damasceno@epicom.com.br>
2023-06-23 11:11:38 -0300

    Design Patterns de Criação - Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Singleton
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Singleton jogador1 = Singleton.GetInstancia;
+            jogador1.Mensagem("Jogador 1: A Bola está comigo nomeio de Campo!");
+
+            Singleton jogador2 = Singleton.GetInstancia;
+            jogador2.Mensagem("Jogador 2: Recebi a bola do Jogador 1!");
+
+            Singleton jogador3 = Singleton.GetInstancia;
+            jogador3.Mensagem("Jogador 3: Recebi a bola do Jogador 2!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # design-patterns
 Design Patterns
+
+Projeto de Estudo com Técnicas Avançadas para Desenvolvimento de Software
+
+1 - Design Patterns de Criação
+	1.1 - Singleton
+	
